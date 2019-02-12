09983e28dd358132945ce5635ad90037a630089d
Vanderley Sousa da Silva Junior <vanderley_1998@hotmail.com.br>
2019-02-11 22:38:49 -0200

    implementacao de funções js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,58 @@
 Contatos = {
-
     mostrarTodos() {
-        const favs = document.querySelectorAll("#lista_itens a")
-        favs.forEach(e => {
-            if(!e.getAttribute('wm-favorito')){
-                e.style.display = 'block';
-            }
+        const todos = document.querySelectorAll(".lista_itens a")
+        todos.forEach(e => {
+            e.removeAttribute('style')
         })
     },
     mostrarFavoritos() {
-        const favs = document.querySelectorAll("#lista_itens a")
+        const favs = document.querySelectorAll(".lista_itens a")
         favs.forEach(e => {
-            if(!e.getAttribute('wm-favorito')){
+            if (!e.getAttribute('wm-favorito')) {
                 e.style.display = 'none';
             }
         })
+    },
+    buscarContatos(nome) {
+        this.mostrarTodos()
+        const todos = document.querySelectorAll(".nome_contato")
+        if (nome) {
+            todos.forEach(e => {
+                if (!e.innerHTML.includes(nome)) {
+                    e.parentNode.parentNode.style.display = 'none'
+                }
+            })
+        }
+    },
+    favoritar(element) {
+        console.log('Teste:', element.parentNode.parentNode.getAttribute('wm-favorito'))
+        flag = (element.parentNode.parentNode.getAttribute('wm-favorito')) ? false : true
+        if (flag) {
+            element.src = './assets/img/baseline-favorite-24px.svg'
+            element.title = 'Desfavoritar'
+            element.parentNode.parentNode.setAttribute('wm-favorito', 'true')
+        } else {
+            element.src = './assets/img/baseline-favorite_border-24px.svg'
+            element.title = 'Favoritar'
+            element.parentNode.parentNode.removeAttribute('wm-favorito')
+        }
     }
+}
 
+window.onload = function () {
+    document.getElementsByName('filtro')[0].onclick = function () {
+        Contatos.mostrarTodos()
+    }
+    document.getElementsByName('filtro')[1].onclick = function () {
+        Contatos.mostrarFavoritos()
+    }
+    document.getElementsByClassName('pesquisaInput')[0].onkeyup = function () {
+        Contatos.buscarContatos(this.value)
+    }
+    Array.from(document.getElementsByClassName('fav')).forEach(e => {
+        e.onclick = function () {
+            Contatos.favoritar(this)
+        }
+    })
+    Contatos.preventEventFav()
 }
