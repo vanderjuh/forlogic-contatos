c6a17d111b6a3d42500a6db626814372e66b6374
Vanderley Sousa <vanderley_1998@hotmail.com.br>
2019-02-27 09:42:07 -0300

    lista fake

--- src/app/lista-contatos/lista-contatos.component.ts
+++ src/app/lista-contatos/lista-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import IconFavoritado from '../../assets/img/baseline-favorite-24px.svg'
 
 @Component({
   selector: 'app-lista-contatos',
@@ -6,6 +7,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-contatos.component.css']
 })
 export class ListaContatosComponent implements OnInit {
+
+  iconFavoritado: any = IconFavoritado;
+
+  listaContatos: any[] = [
+    {
+      id: 1,
+      firstName: 'Vanderley',
+      lastName: 'Sousa da Silva Junior',
+      email: 'vanderley@forlogic.net',
+      gender: 'm',
+      isFavorite: false,
+      info: {
+        id: 1,
+        company: 'Forlogic',
+        // tslint:disable-next-line:max-line-length
+        avatar: 'https://trello-attachments.s3.amazonaws.com/5c59aed4db6b3938eaa9e254/300x300/f70f3c1d8ff9481cd524b664587f3973/vanderley-02.jpg',
+        address: 'Rua A',
+        phone: '43991841963',
+        comments: 'dev'
+      },
+      created: '2019-02-27T11:44:56.773Z'
+    },
+    {
+      id: 1,
+      firstName: 'Vanderley',
+      lastName: 'Sousa da Silva Junior',
+      email: 'vanderley@forlogic.net',
+      gender: 'm',
+      isFavorite: true,
+      info: {
+        id: 1,
+        company: 'Forlogic',
+        // tslint:disable-next-line:max-line-length
+        avatar: 'https://trello-attachments.s3.amazonaws.com/5c59aed4db6b3938eaa9e254/300x300/f70f3c1d8ff9481cd524b664587f3973/vanderley-02.jpg',
+        address: 'Rua A',
+        phone: '43991841963',
+        comments: 'dev'
+      },
+      created: '2019-02-27T11:44:56.773Z'
+    },
+    {
+      id: 1,
+      firstName: 'Vanderley',
+      lastName: 'Sousa da Silva Junior',
+      email: 'vanderley@forlogic.net',
+      gender: 'm',
+      isFavorite: true,
+      info: {
+        id: 1,
+        company: 'Forlogic',
+        // tslint:disable-next-line:max-line-length
+        avatar: 'https://trello-attachments.s3.amazonaws.com/5c59aed4db6b3938eaa9e254/300x300/f70f3c1d8ff9481cd524b664587f3973/vanderley-02.jpg',
+        address: 'Rua A',
+        phone: '43991841963',
+        comments: 'dev'
+      },
+      created: '2019-02-27T11:44:56.773Z'
+    },
+    {
+      id: 1,
+      firstName: 'Vanderley',
+      lastName: 'Sousa da Silva Junior',
+      email: 'vanderley@forlogic.net',
+      gender: 'm',
+      isFavorite: true,
+      info: {
+        id: 1,
+        company: 'Forlogic',
+        // tslint:disable-next-line:max-line-length
+        avatar: 'https://trello-attachments.s3.amazonaws.com/5c59aed4db6b3938eaa9e254/300x300/f70f3c1d8ff9481cd524b664587f3973/vanderley-02.jpg',
+        address: 'Rua A',
+        phone: '43991841963',
+        comments: 'dev'
+      },
+      created: '2019-02-27T11:44:56.773Z'
+    }
+  ]
 
   constructor() { }
 
